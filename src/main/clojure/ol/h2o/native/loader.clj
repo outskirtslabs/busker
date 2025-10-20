77391c6ef67f119a84d29ddb50b4c9f4aa44e99b
Casey Link <casey@outskirtslabs.com>
2025-10-20 20:27:51 +0200

    Migrate to Zig-based cross-platform build system for native shims
    Restructure build to use Zig for cross-compiling native libraries across
    Linux x86-64, macOS x86-64, and macOS aarch64, with platform-specific
    resource directories and automatic library loading from classpath.

--- src/main/clojure/ol/h2o/native/loader.clj
+++ src/main/clojure/ol/h2o/native/loader.clj
@@ -2,41 +2,45 @@
   (:require
    [clojure.java.io :as io]
    [clojure.string :as str]
-   [coffi.ffi :as ffi]))
+   [coffi.ffi :as ffi])
+  (:import
+   [java.nio.file Files]))
 
 (defn copy-resource [resource-path output-path]
   (with-open [in (io/input-stream (io/resource resource-path))
               out (io/output-stream (io/file output-path))]
     (io/copy in out)))
 
-(defn get-arch+os []
-  (let [os-name (str/lower-case (System/getProperty "os.name"))]
-    (str (System/getProperty "os.arch") "-"
-         (cond (str/includes? os-name "win") "windows"
-               (str/includes? os-name "nux") "linux"
-               (str/includes? os-name "mac") "macos"))))
+(defn get-os-arch []
+  (let [os-name (str/lower-case (System/getProperty "os.name"))
+        os (cond (str/includes? os-name "win") "windows"
+                 (str/includes? os-name "nux") "linux"
+                 (str/includes? os-name "mac") "macos")
+        arch-raw (System/getProperty "os.arch")
+        arch (cond (= arch-raw "amd64") "x86-64"
+                   (= arch-raw "x86_64") "x86-64"
+                   (= arch-raw "aarch64") "aarch64"
+                   :else arch-raw)]
+    (str os "-" arch)))
 
-#_(defn load-bundled-library []
-    (let [res-file (case (get-arch+os)
-                     "aarch64-linux" "libh2o_aarch64-linux-gnu.so"
-                     "aarch64-macos" "libh2o_aarch64-macos-none.so"
-                     ("x86-linux"
-                      "amd64-linux") "libh2o_x86_64-linux-gnu.so"
-                     ("x86-macos"
-                      "amd64-macos") "libh2o_x86_64-macos-none.so"
-                     ("x86-windows"
-                      "amd64-windows") "libh2o_x86_64-windows-gnu.dll")
-          temp-lib-filename (str "h2oclj_temp_" res-file)]
-      (copy-resource res-file temp-lib-filename)
-      (ffi/load-library temp-lib-filename)
-      (Files/deleteIfExists (.toPath (io/file temp-lib-filename)))))
+(defn load-bundled-library []
+  (let [platform (get-os-arch)
+        _ (when (str/includes? platform "windows") (throw (Exception. "Windows is not supported, please use wsl.")))
+        lib-name (if (str/includes? platform "macos")
+                   "libh2oclj.dylib"
+                   "libh2oclj.so")
+        resource-path (str platform "/" lib-name)
+        temp-dir (System/getProperty "java.io.tmpdir")
+        temp-lib-path (str temp-dir "/h2oclj_" (System/currentTimeMillis) "_" lib-name)]
+    #_(println "resource path" resource-path)
+    (try
+      (copy-resource resource-path temp-lib-path)
+      (ffi/load-library temp-lib-path)
+      (finally
+        (try
+          (Files/deleteIfExists (.toPath (io/file temp-lib-path)))
+          (catch Exception _))))))
 
-#_(defn load-system-library []
-    (ffi/load-system-library "libh2o"))
-
-;; (ffi/load-library (System/getProperty "ol.libh2o.path"))
-(try
-  (ffi/load-library "zig-out/lib/libh2o-evloop.so")
-  (ffi/load-library "zig-out/lib/libh2oclj.so")
-  (catch UnsatisfiedLinkError e
-    (throw (Exception. "unable to load native h2o library; is it installed?" e))))
+(if-let [lib-path (System/getProperty "ol.libh2oclj.path")]
+  (ffi/load-library lib-path)
+  (load-bundled-library))
