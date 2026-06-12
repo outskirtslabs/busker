4bb0cb8db2d72b9b683b4c58c28df819d35815c4
Casey Link <casey@outskirtslabs.com>
2026-06-12 12:52:16 +0200

    fix nix flake check

--- pkgs/busker.nix
+++ pkgs/busker.nix
@@ -70,7 +70,6 @@ clojureLib.mkCljLib {
     cljfmt check src
     clj-kondo --lint src
     clojure -Srepro -M:dev:test:kaocha
-    clojure -Srepro -J-Xmx2g -J-Xms2g -M:dev:test:kaocha --no-capture-output --focus ol.busker.large-payload-test
   '';
   nativeBuildInputs = [
     curlWithSsls
